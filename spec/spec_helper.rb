c13e91fc2befdf22df2b762345ef3730d41f1ec2
Pedro Januário <prnjanuario@gmail.com>
2014-07-29 16:32:14 +0100

    added spec helper with configs for code coverage

---
+++ spec/spec_helper.rb
@@ -0,0 +1,25 @@
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+else
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter '/spec/'
+  end
+end
+
+$:.push '.'
+
+require 'rspec'
+require 'pry'
+require 'logger-facade'
+
+Dir['spec/support/**/*.rb'].each &method(:require)
+
+RSpec.configure do |c|
+  c.around(:each) do |example|
+    Timeout::timeout(2) {
+      example.run
+    }
+  end
+end
